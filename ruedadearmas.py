c969991c9096482b88176b57ad71bb3955133d1f
MarioSerradillaPenido <146455953+MarioSerradillaPenido@users.noreply.github.com>
2023-11-15 19:08:07 +0100

    Update ruedadearmas.py

--- ruedadearmas.py
+++ ruedadearmas.py
@@ -1,7 +1,12 @@
 class arma:
-    def __init__(self, tipo, nombre, ammo, daño, recarga):
+    def __init__(self, tipo, nombre, ammo, daño, recarga, rango):
         self.tipo = tipo
         self.nombre = nombre
         self.ammo = ammo
         self.daño = daño
         self.recarga = recarga
+        self.rango
+
+class pistola(arma):
+    def __init__(self, tipo, nombre, ammo, daño, recarga, rango):
+        super().__init__(tipo, nombre, ammo, daño, recarga, rango)
