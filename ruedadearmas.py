61947318e089bc433813e920406efa64ce369344
MarioSerradillaPenido <146455953+MarioSerradillaPenido@users.noreply.github.com>
2023-11-15 19:03:39 +0100

    Add files via upload

---
+++ ruedadearmas.py
@@ -0,0 +1,7 @@
+class arma:
+    def __init__(self, tipo, nombre, ammo, daño, recarga):
+        self.tipo = tipo
+        self.nombre = nombre
+        self.ammo = ammo
+        self.daño = daño
+        self.recarga = recarga
